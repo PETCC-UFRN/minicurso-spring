e3d42c3f0a4197220ab581b71b677afbe127880e
schwaad <joseivoschwadea@gmail.com>
2026-02-06 10:33:29 -0300

    mudados os nomes dos dias 3 e 4 e reativado o script de só ativar a página quando chegar o dia.

--- assets/js/script_dia.js
+++ assets/js/script_dia.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   const datasAtivacao = {
-    "primeiroDia": new Date("2025-03-07"), // A partir de 07/03/2025
-    "segundoDia": new Date("2025-03-11"), // A partir de 11/03/2025
-    "terceiroDia": new Date("2025-03-12"), // A partir de 12/03/2025
-    "quartoDia": new Date("2025-03-13"), // A partir de 13/03/2025
-    "quintoDia": new Date("2025-03-14"), // A partir de 14/03/2025
+    "primeiroDia": new Date("2026-02-23"), // A partir de 07/03/2025
+    "segundoDia": new Date("2026-02-24"), // A partir de 11/03/2025
+    "terceiroDia": new Date("2026-02-25"), // A partir de 12/03/2025
+    "quartoDia": new Date("2026-02-26"), // A partir de 13/03/2025
+    "quintoDia": new Date("2026-02-27"), // A partir de 14/03/2025
     "projetoFinalSimplificado": new Date("2025-03-14") // A partir de 14/03/2025
   };
 
